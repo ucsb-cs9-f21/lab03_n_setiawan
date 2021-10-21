1e4174512d7e26102b948d2024d61a4f7ed477c5
nealsonSetiawan <91297327+nealsonSetiawan@users.noreply.github.com>
2021-10-21 14:49:18 -0700

    Add files via upload

--- lab03.py
+++ lab03.py
@@ -11,7 +11,7 @@ def multiply(x,y):
     else:
         return 0
 
-def collectOddValues(listOfInt):
+#def collectOddValuesOld(listOfInt):
     x = 0
     for i in range(len(listOfInt)):
         if(listOfInt[i] % 2 != 1):
@@ -31,8 +31,16 @@ def collectOddValues(listOfInt):
     else:
         return listOfInt
 
-
-def countInts(listOfInt, num):
+def collectOddValues(listOfInt):
+    n = []
+    if(listOfInt != []):
+        if(listOfInt[0]%2 == 1):
+            n.append(listOfInt[0])
+        
+        return n + collectOddValues(listOfInt[1:])
+    else:
+        return []
+#def countIntsOld(listOfInt, num):
     x = 0
     for i in range(len(listOfInt)):
         if(listOfInt[i] != num):
@@ -51,5 +59,40 @@ def countInts(listOfInt, num):
     else:
         return len(listOfInt)
 
+def countInts(listOfInt, num):
+    n = 0
+    if(listOfInt != []):
+        if listOfInt[0] == num:
+            n = 1
+        return n + countInts(listOfInt[1:],num)
+
+    else:
+        return 0
+
+def reverseString(s):
+    if(s != ""):
+        c = s[len(s)-1]
+        return c + reverseString(s[0:len(s)-1])
+    else:
+        return ""
+
+def removeSubString(s,sub):
+    z = ""
+    k = ""
+    if(s != ""):
+        c = s[0:len(sub)]
+        if c == sub:
+            z = s[len(sub):len(s)]
+        else:
+            k = s[0]
+            z = s[1:]
+        
+        return k + removeSubString(z,sub)
+    else:
+        return ""
+
+assert removeSubString("Lololololmm", "lol") == "Loomm"
 assert countInts([1,2,3,4,3,2,1], 2) == 2
 assert collectOddValues([0,1,2,3,4,5]) == [1,3,5]
+assert reverseString("Hello") == "olleH"
+assert reverseString("CMPSC9") == "9CSPMC"
